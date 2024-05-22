19282e02c349d824cfaab56486ca8d2d79bd554a
Kyle <stonewal7@outlook.com>
2024-05-22 16:40:36 -0700

    a bit of code refactoring

--- site/websocket.cpp
+++ site/websocket.cpp
@@ -44,45 +44,45 @@ void Web::WebsocketServer::print_frame(uint8_t frame[], int len) {
 //have this return some kind of value (bool, int) to signify value recieved
 void Web::WebsocketServer::begin() {
     if (this->cbSet == 1) {
+        //create socket
         bind(this->listenSocket, (struct sockaddr*)&this->serverAddr, sizeof(this->serverAddr));
         listen(this->listenSocket, 5);
 
+        //listen for connections
         this->clientSocket = accept(this->listenSocket, &clientAddr, (socklen_t*)&clientAddrSize);
 
-        const int bufSize = 1024;
-        char *buffer = (char*)malloc(bufSize*sizeof(char));
-        char *readBuffer = (char*)malloc(bufSize*sizeof(char));
+        char *buffer = (char*)malloc(this->maxPktSize*sizeof(char));
+        char *readBuffer = (char*)malloc(this->maxPktSize*sizeof(char));
           
         std::cout << "Reading input\n";
         std::cout << sizeof(readBuffer);
-        if(read(this->clientSocket, readBuffer, bufSize) == 0) {
+        if(read(this->clientSocket, readBuffer, this->maxPktSize) == 0) {
             std::cout << "Read does not wokr?\n";
         }
 
         int headerSize;
-        char *wsHeader = create_ws_header(readBuffer, bufSize, headerSize);
+        char *wsHeader = create_ws_header(readBuffer, this->maxPktSize, headerSize);
 
         if(wsHeader != NULL) {
             std::cout << "Init WS upgrade\n";
-            memset(buffer, '\0', bufSize);
+            memset(buffer, '\0', this->maxPktSize);
             send(this->clientSocket, wsHeader, headerSize-1, 0);
-            recv(this->clientSocket, buffer, bufSize, 0);
+            recv(this->clientSocket, buffer, this->maxPktSize, 0);
 
             //use ping/pong frame to test connection is alive
             //Check if connection has been terminated
-            //read(this->listenSocket, readBuffer, bufSize);
-            //print_frame(readBuffer, bufSize);
+            //read(this->listenSocket, readBuffer, this->maxPktSize);
+            //print_frame(readBuffer, this->maxPktSize);
 
            
-            //uint8_t *connBuf = (uint8_t*)malloc(bufSize*sizeof(uint8_t));
+            //uint8_t *connBuf = (uint8_t*)malloc(this->maxPktSize*sizeof(uint8_t));
             //detect if websocket connection was successful
             //listener();
             
             if(buffer[0] != 0) {
                 std::cout << "Websocket Connection Successful\n";
-                uint8_t msg[bufSize];
-                memset(msg, '\0', bufSize);
-
+                uint8_t msg[this->maxPktSize];
+                memset(msg, '\0', this->maxPktSize);
                 listener();
             }
             else {
@@ -136,12 +136,12 @@ int Web::WebsocketServer::get_websocket_key(char *header, const int headerSize, 
 
 
 char *Web::WebsocketServer::create_ws_header(char *buf, int size, int &hSize) {
-    char initReg[] = "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ";
     uint8_t webkey[60];
     memset(webkey, '\0', sizeof(webkey));
     char *wsHeader;
 
     if(get_websocket_key(buf, size, webkey, sizeof(webkey)) == 0) {
+        char initReg[] = "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ";
         int baseSize = base64_length(SHA_DIGEST_LENGTH)+4; //add 4 to include space for \r\n\r\n packet ender
         
         uint8_t base64[baseSize];
@@ -149,11 +149,9 @@ char *Web::WebsocketServer::create_ws_header(char *buf, int size, int &hSize) {
         uint8_t *hashBuf = (uint8_t*)malloc(hashSize*sizeof(uint8_t));
 
         gen_sha_hash(webkey, sizeof(webkey), hashBuf);
-
         gen_base64(hashBuf, hashSize, base64, baseSize-4);
        
-
-        //THIS HAS BEEN CHANGED FOR TESTING PURPOSES.
+        //indicate end of packet
         memcpy(&base64[baseSize-4], "\r\n\r\n", 4);
 
         int headerSize = sizeof(initReg) + baseSize;
@@ -199,10 +197,8 @@ int Web::WebsocketServer::recv_data(char *buffer, int bufSize, uint8_t msg[], in
             memcpy(key, &buffer[2], 4);
             len = lenIndic;
            
-            if(lenIndic < 255) {
-                encoded = (uint8_t*)malloc(len*sizeof(uint8_t));
-                memcpy(encoded, &buffer[6], len);
-            }
+            encoded = (uint8_t*)malloc(len*sizeof(uint8_t));
+            memcpy(encoded, &buffer[6], len);
         }
         else if(lenIndic == 126) { // Check if length is stored as two bytes
             memcpy(key, &buffer[4], 4);
@@ -228,14 +224,12 @@ int Web::WebsocketServer::recv_data(char *buffer, int bufSize, uint8_t msg[], in
             msg[i] = (encoded[i] ^ key[i & 0x3]);
         }
         free(encoded);
-        std::cout << "Length: " << len << "\n";
         //Moved to inside the if statment to prevent large unchecked values from being returned
         return len; 
     }
     else {
         std::cout << "No 129\n";
     }
-    std::cout << "Length 2: " << len << "\n";
 
     return -2;
 }
@@ -246,8 +240,6 @@ void Web::WebsocketServer::create_frame(uint8_t buf[], char msg[], int len) {
     //add check for packet size later
 
     if(len < 126) {
-        //buf[0] = 0b10000001;
-
         //use strncpy to create frame. May be less variabality than memset?
         buf[0] = 129;
         //buf[1] = (unsigned char)len; //set mask bit to 0, and indicate message length
@@ -255,7 +247,6 @@ void Web::WebsocketServer::create_frame(uint8_t buf[], char msg[], int len) {
 
         //make sure len includes just the message and not any \r\n that may come after
         memcpy(&buf[2], msg, len); //copy message to buffer
-        //memcpy(&buf[2+len], "\r\n\r\n", 4); //end the packet
         std::cout << "Frame created\n";
     }
 }
@@ -276,41 +267,27 @@ void Web::WebsocketServer::send_data(char msg[], int len) {
 //listen for incoming messages
 int Web::WebsocketServer::listener() {
     std::cout << "Listening for messages...\n";
-    int bufSize = 1024;
-    char *buffer = (char*)malloc(bufSize*sizeof(char));
-    uint8_t msg[bufSize];
-    memset(msg, '\0', bufSize);
-    memset(buffer, '\0', bufSize);
+    char *buffer = (char*)malloc(this->maxPktSize*sizeof(char));
+    uint8_t msg[this->maxPktSize];
+    memset(msg, '\0', this->maxPktSize);
+    memset(buffer, '\0', this->maxPktSize);
     int g;
 
     //delete after done testing
-    uint8_t testPkt[] = {0b10000001, 4, 'h', 'e', 'l', 'l'};
 
-    while((g = recv(this->clientSocket, buffer, bufSize, 0)) > 0) {
-        //std::cout << "\n\nListening...\n";
+    while((g = recv(this->clientSocket, buffer, this->maxPktSize, 0)) > 0) {
         std::cout << "message recieved\n";
-        //send(this->clientSocket, packet, 11, 0);
         if(buffer[0] != '\0') {
-            int size = recv_data(buffer, bufSize, msg, bufSize);
+            int size = recv_data(buffer, this->maxPktSize, msg, this->maxPktSize);
             std::cout << "Size test: " << size << "\n";
             if(size > 0) {
                 char tstMsg[] = "hello";
                 func_cb(msg, size);
                 send_data(tstMsg, sizeof(tstMsg)-1);
-                //sleep(1);
-                //send(this->clientSocket, testPkt, 6, 0);
-                //testPkt[2] = 'x';
-                //sleep(1);
-                //std::cout << "Sending second packet\n";
-                //send(this->clientSocket, testPkt, 6, 0);
-                
-                //std::cout << "sent\n";
-
-                //send_data(testFrm, sizeof(testFrm));
                 
                 //reset buffers
-                memset(msg, '\0', bufSize);
-                memset(buffer, '\0', bufSize);
+                memset(msg, '\0', this->maxPktSize);
+                memset(buffer, '\0', this->maxPktSize);
             }
         }
     }
