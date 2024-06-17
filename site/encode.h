b6dd3c4804d4d3b352c802dec4721483e90bcd22
kyle <stonewal7@outlook.com>
2024-06-16 20:58:17 -0700

    added for loop?

--- site/encode.h
+++ site/encode.h
@@ -6,7 +6,7 @@
 #include <iostream>
 
 class Encode {
-    char constexpr static encode_table[] {
+    char constexpr static encode_table[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
         'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
         'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
